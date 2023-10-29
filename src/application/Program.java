8ab7c92fa56ce4bc828ea03d499e0f5e9e6f2291
fernandocarvalhaes007 <fernandosantos07@gmail.com>
2023-10-29 10:47:54 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,17 @@
 package application;
 
+
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 
-		
-		  System.out.print("Hello Word");
+		Position pos = new Position(3 , 5);
 		
 		
 	}
 
 }
+  
